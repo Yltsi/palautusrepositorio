24b0325c98dfaf9aa530dee76b8f3100406722fe
Yltsi <ylinemi@gmail.com>
2024-12-20 20:02:25 +0200

    kps done

--- viikko7/kivi-paperi-sakset/src/index.py
+++ viikko7/kivi-paperi-sakset/src/index.py
@@ -2,7 +2,6 @@ from kps_pelaaja_vs_pelaaja import KPSPelaajaVsPelaaja
 from kps_tekoaly import KPSTekoaly
 from kps_parempi_tekoaly import KPSParempiTekoaly
 
-
 def main():
     while True:
         print("Valitse pelataanko"
@@ -14,30 +13,17 @@ def main():
 
         vastaus = input()
 
-        if vastaus.endswith("a"):
-            print(
-                "Peli loppuu kun pelaaja antaa virheellisen siirron eli jonkun muun kuin k, p tai s"
-            )
-
-            kaksinpeli = KPSPelaajaVsPelaaja()
-            kaksinpeli.pelaa()
-        elif vastaus.endswith("b"):
-            print(
-                "Peli loppuu kun pelaaja antaa virheellisen siirron eli jonkun muun kuin k, p tai s"
-            )
-
-            yksinpeli = KPSTekoaly()
-            yksinpeli.pelaa()
-        elif vastaus.endswith("c"):
-            print(
-                "Peli loppuu kun pelaaja antaa virheellisen siirron eli jonkun muun kuin k, p tai s"
-            )
-
-            haastava_yksinpeli = KPSParempiTekoaly()
-            haastava_yksinpeli.pelaa()
+        if vastaus == "a":
+            peli = KPSPelaajaVsPelaaja()
+        elif vastaus == "b":
+            peli = KPSTekoaly()
+        elif vastaus == "c":
+            peli = KPSParempiTekoaly()
         else:
             break
 
+        print("Peli loppuu kun pelaaja antaa virheellisen siirron eli jonkun muun kuin k, p tai s")
+        peli.pelaa()
 
 if __name__ == "__main__":
     main()
